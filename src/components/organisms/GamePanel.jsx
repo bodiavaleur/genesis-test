39dc8b86bb044f92454ab25b712613328c2ce884
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-11-11 01:18:59 +0200

    Refactored game panel

--- src/components/organisms/GamePanel.jsx
+++ src/components/organisms/GamePanel.jsx
@@ -1,26 +1,68 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Option, Question } from "../atoms";
-import { List } from "../molecules";
-import questions from "../../questions.json";
+import React from "react";
+import { AnswersTip, Question } from "../atoms";
+import { List, Option } from "../molecules";
+import { Redirect } from "react-router-dom";
+import styled from "styled-components";
 
-export function GamePanel() {
-  const dispatch = useDispatch();
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+const QuestionWrapper = styled.div`
+  display: flex;
+  min-height: 6rem;
+  flex-direction: column;
+  justify-content: space-between;
 
-  const clickAnswer = (evt) => {
-    const answer = evt.target.name;
-  };
+  text-align: center;
+
+  @media screen and (min-width: 425px) {
+    max-width: 30rem;
+    text-align: start;
+  }
+`;
+
+export function GamePanel({
+  correct,
+  pending,
+  wrong,
+  selectedAnswers,
+  clickAnswer,
+  question,
+  gameOver,
+}) {
+  const showQuestion = (answers) =>
+    answers.map((answer, idx) => {
+      // destructure entries of answer to get key of answer and actual answer
+      const [key, answ] = Object.entries(answer)[0];
+      return (
+        <Option
+          selected={selectedAnswers.includes(key)}
+          correct={correct && selectedAnswers.includes(key)}
+          pending={pending && selectedAnswers.includes(key)}
+          wrong={wrong && selectedAnswers.includes(key)}
+          key={idx}
+          option={key.toUpperCase()}
+          answer={answ}
+          select={() => clickAnswer(key)}
+        />
+      );
+    });
+
+  // To show count of possible answers
+  const correctAnswersLength = question?.rightAnswers.length;
 
   return (
     <>
-      <Question>
-        How old your elder brother was 10 years before you was born, mate?
-      </Question>
-      <List>
-        {}
-        <Option />
-      </List>
+      {question && (
+        <>
+          <QuestionWrapper>
+            <Question>{question.question}</Question>
+            {correctAnswersLength > 1 && (
+              <AnswersTip>Possible answers: {correctAnswersLength}</AnswersTip>
+            )}
+          </QuestionWrapper>
+          <List>{showQuestion(question.answers)}</List>
+        </>
+      )}
+
+      {gameOver && <Redirect to='/results' />}
     </>
   );
 }
