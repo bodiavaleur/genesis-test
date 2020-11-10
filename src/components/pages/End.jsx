5f3754f9bf1b2543a0b89d42d6ba7e9927f18dd5
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-11-11 01:20:05 +0200

    Made final page in case of win/lose

--- src/components/pages/End.jsx
+++ src/components/pages/End.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setPrize } from "../../redux/actionCreators";
 import { Overview } from "../templates";
 
 export function End() {
+  const prize = useSelector((state) => state.prize);
+  const formatPrize = prize.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(setPrize(0));
+    };
+  }, []);
+
   return (
     <Overview
       score
-      title='$8000 earned'
+      title={`$${formatPrize} earned`}
       buttonLabel='Try again'
       redirectTo='/'
     />
